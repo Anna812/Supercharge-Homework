788dbd389ae0779df545435933da4cbe3283695e
Anna812 <88menyus@gmail.com>
2018-05-02 15:42:16 +0200

    add basic file reader logic inFileIO helper class

--- src/Helper/FileIO.java
+++ src/Helper/FileIO.java
@@ -1,8 +1,25 @@
 package Helper;
 
-public class FileIO {
-    public void readTransactionsFromFile() {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class FileIO {
+    public String readTransactionsFromFile() {
+        String fileContent = "";
+        try(BufferedReader br = new BufferedReader(new FileReader("resources/TransactionHistory.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            fileContent = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 
     public void writeTransactionsToFile() {
