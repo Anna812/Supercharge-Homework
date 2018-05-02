f7cf3e1779b45d6964e5de9d6c7cf1b363b7b60d
Anna812 <88menyus@gmail.com>
2018-05-02 15:51:17 +0200

    create basic write logic in FileIO class

--- src/Helper/FileIO.java
+++ src/Helper/FileIO.java
@@ -2,10 +2,17 @@ package Helper;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileIO {
     private static final String filePath = "resources/TransactionHistory.txt";
-    
+
     public String readTransactionsFromFile() {
         String fileContent = "";
         try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -24,7 +31,14 @@ public class FileIO {
         return fileContent;
     }
 
-    public void writeTransactionsToFile() {
-
+    public void writeTransactionsToFile(String transaction) {
+        List<String> input = Arrays.asList(transaction);
+        Path file = Paths.get(filePath);
+        
+        try {
+            Files.write(file, input, Charset.forName("UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
