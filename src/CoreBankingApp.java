e269a1490dbd16bc8217b3662e5e8b26e33cc926
Anna812 <88menyus@gmail.com>
2018-05-02 15:10:20 +0200

    create basic command line mechanism

--- src/CoreBankingApp.java
+++ src/CoreBankingApp.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class CoreBankingApp {
+    static Scanner userInput =  new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        String info = "Welcome at your core banking service. Type \"d\" to deposit money. \n" +
+                "Type \"w\" to withdraw money. \n" +
+                "Type \"t\" to transfer money. \n" +
+                "Type \"h\" to see your accounts history. \n" +
+                "Type \"q\" to stop the program";
+
+        System.out.println(info);
+        while(userInput.hasNext()) {
+            String input = userInput.next();
+            if(input.equals("d")){
+                //deposit logic
+            } else if (input.equals("w")) {
+                // withdraw logic
+            } else if (input.equals("t")) {
+                // transfer logic
+            } else if (input.equals("h")) {
+                // history logic
+            } else if (input.equals("q")) {
+                System.out.println("Thank you for using our service!");
+                break;
+            } else {
+                System.out.println(info);
+            }
+        }
     }
 }
