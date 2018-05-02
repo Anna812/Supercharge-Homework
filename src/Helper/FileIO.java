1b185a995b579be391d7de1997b3ff9b11fbc6db
Anna812 <88menyus@gmail.com>
2018-05-02 16:11:11 +0200

    change file reader method to return arraylist

--- src/Helper/FileIO.java
+++ src/Helper/FileIO.java
@@ -2,29 +2,26 @@ package Helper;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileIO {
     private static final String filePath = "resources/TransactionHistory.txt";
 
-    public String readTransactionsFromFile() {
-        String fileContent = "";
+    public ArrayList<String> readTransactionsFromFile() {
+        ArrayList<String> fileContent = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
+                fileContent.add(line);
                 line = br.readLine();
             }
-            fileContent = sb.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
