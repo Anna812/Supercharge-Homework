6407b9cafa46284160c4ebe33f013fa1d8321989
Anna812 <88menyus@gmail.com>
2018-05-02 16:39:25 +0200

    implement deposit method, realise the flaws of FileIO write method

--- src/BankingLogic/Logic.java
+++ src/BankingLogic/Logic.java
@@ -1,19 +1,25 @@
 package BankingLogic;
 
+import Helper.FileIO;
 import Models.Account;
+import Models.Transaction;
 
 import java.util.ArrayList;
 
 public class Logic implements ILogic {
     private Account account;
+    private FileIO fileIO;
 
     public Logic() {
       account = new Account();
+      fileIO = new FileIO();
     }
 
     @Override
     public void deposit(int amount) {
-
+        account.balance = account.balance - amount;
+        Transaction newDeposit = new Transaction("Deposit", amount);
+        fileIO.writeTransactionsToFile(newDeposit);
     }
 
     @Override
